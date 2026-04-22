7d9a4c5776503bf1be9bd39f6660a889b9b8fcee
davidcurbet111111111 <davidcurbet111111111@protonmail.com>
2026-04-22 12:22:35 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -32,7 +32,7 @@ echo "$USER:$PASS" | sudo chpasswd
 sudo usermod -aG sudo $USER
 
 # 5. Установка и запуск SSH
-sudo apt-get update && sudo apt-get install -y openssh-server
+sudo apt update && sudo apt install -y openssh-server
 sudo systemctl enable ssh
 sudo systemctl start ssh
 
